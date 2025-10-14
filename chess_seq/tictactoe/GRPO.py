fixed causal mask bug

chess_seq/tictactoe/GRPO.py
@@ -47,6 +47,7 @@ class GRPO(TTTAgent):
         end_lr_steps,
         device=None,
         writer: SummaryWriter = None,
+        prints=False,
     ):
         self.full_name = base_name + "_GRPO"
         super().__init__(model, encoder, full_name=self.full_name, device=device)
@@ -71,6 +72,7 @@ class GRPO(TTTAgent):
         self.min_lr = min_lr
         self.device = device
         self.writer = writer
+        self.prints = prints
 
         self.reset()
 
@@ -131,6 +133,9 @@ class GRPO(TTTAgent):
     def optimizer_step(self):
         log_ratios = self.compute_log_ratios()  # (G, L)
         masks = self.compute_mask()  # (G, L)
+        if self.prints:
+            print(f"{masks=}")
+            print("----------------------")
 
         rewards = torch.cat(self.group_rewards).unsqueeze(1)  # (G, 1)
 
@@ -140,15 +145,12 @@ class GRPO(TTTAgent):
         else:
             advantages = (rewards - rewards.mean()) / (rewards.std() + 1e-3)
 
-        score1 = torch.exp(log_ratios) * advantages  # (G, L)
-        score2 = (
-            torch.clamp(
-                torch.exp(log_ratios), 1 - self.epsilon_low, 1 + self.epsilon_high
-            )
-            * advantages
-        )  # (G, L)
+        ratios = torch.exp(log_ratios)  # (G, L)
+        score1 = ratios * advantages
+        score2 = ratios.clamp(1 - self.epsilon_low, 1 + self.epsilon_high) * advantages
+        # (G, L)
 
-        # Masking flattens/ modern version
+        # Masking flattens / modern version: all tokens count equally
         unregularized_score = torch.minimum(score1, score2)[masks].mean()
         KL = (torch.exp(-log_ratios) + log_ratios - 1)[masks].mean()
 
@@ -164,8 +166,8 @@ class GRPO(TTTAgent):
         self.log_training(
             KL.item(),
             unregularized_score.item(),
-            rewards.numpy().mean(),
-            rewards.numpy().std(),
+            rewards.cpu().numpy().mean(),
+            rewards.cpu().numpy().std(),
         )
 
     def _clean_group_data(self):
@@ -185,16 +187,17 @@ class GRPO(TTTAgent):
         group_tokenids = pad_sequence(
             self.group_tokenids, batch_first=True, padding_value=self.engine.end_tokenid
         )  # (G, L)
-        # print(f"{group_sequence.shape=}")
-        # print(f"{group_tokenids.shape=}")
+        if self.prints:
+            print(f"{group_sequence=}")
+            print(f"{group_tokenids=}")
 
         log_probs = self.group_log_probs(
-            group_sequence, group_tokenids, self.group_agentids, self.updated_model
+            group_sequence,
+            group_tokenids,
+            self.group_agentids,
+            self.updated_model,
+            prints=self.prints,
         )  # (G, L)
-
-        # print(f"{group_sequence.numpy()=}")
-        # print(f"{group_tokenids.numpy()=}")
-        # print(f"{self.group_agentids=}")
 
         with torch.no_grad():
             old_log_probs = self.group_log_probs(
@@ -208,9 +211,12 @@ class GRPO(TTTAgent):
         tokenids: torch.Tensor,
         agent_ids: torch.Tensor,
         model: torch.nn.Module,
+        prints=False,
     ):
         """ """
-        unn_log_probs = model(sequence)  # G, T, V
+        T = sequence.shape[1]
+        causal_mask = torch.tril(torch.ones(T, T)).to(self.device).bool()
+        unn_log_probs = model(sequence, mask=causal_mask)  # G, T, V
         log_probs = torch.log_softmax(unn_log_probs, dim=-1)
 
         start_indices = torch.tensor(
@@ -218,6 +224,13 @@ class GRPO(TTTAgent):
             dtype=torch.int64,
             device=sequence.device,
         )
+        if prints:
+            print(f"{agent_ids=}")
+            print(f"{start_indices=}")
+            with torch.no_grad():
+                print(
+                    f"Tokens grabbed in 'sequence' \n {self.grab_play_indices(start_indices, sequence.unsqueeze(-1))}"
+                )
 
         played_log_probs = self.grab_play_indices(start_indices, log_probs)  # (G, L, V)
 
@@ -234,7 +247,10 @@ class GRPO(TTTAgent):
 
         max_len = log_probs.shape[1]
         L = max_len // 2 + max_len % 2
-        idx = torch.arange(L).unsqueeze(0) * 2 + start_indices.unsqueeze(1)  # (G, L)
+        idx = torch.arange(L, device=self.device).unsqueeze(
+            0
+        ) * 2 + start_indices.unsqueeze(1)
+        # (G, L)
         idx = idx.unsqueeze(-1).expand(-1, -1, log_probs.shape[2])  # (G, L, vocab_size)
         idx = idx.clamp(0, max_len - 1)
         return log_probs.gather(1, idx)  # (G, L, vocab_size)

chess_seq/tictactoe/game_engine.py
@@ -45,21 +45,25 @@ class TTTGameEngine:
 
     @torch.no_grad()
     def generate_next_tokenid(
-        self, sequence: torch.Tensor, greedy=True, predict_winner=False, mask=None
+        self,
+        sequence: torch.Tensor,
+        temperature=0.0,
+        predict_winner=False,
+        mask=None,
     ):
         candidate_sequence = sequence.clone()
         out = self.model(candidate_sequence)  # B, T, vocab_size
         logits = out[0, -1]  # vocab_size
         if mask is not None:
             logits = logits + mask
-        if greedy:
+        if temperature == 0:
             next_tokenid = logits.argmax(dim=-1).unsqueeze(0).unsqueeze(0)
             entropy = 0
         else:
             if predict_winner:
                 # set logits to zero for all tokens that are not winners
                 pass
-            dist = torch.distributions.Categorical(logits=logits)
+            dist = torch.distributions.Categorical(logits=logits / temperature)
             entropy = dist.entropy().item()
             next_tokenid = dist.sample().unsqueeze(0).unsqueeze(0)
         candidate_sequence = torch.cat((candidate_sequence, next_tokenid), dim=1)
@@ -67,9 +71,9 @@ class TTTGameEngine:
 
 
 class TTTGamePlayer:
-    def __init__(self, model, encoder, game=None, device=None, greedy=True):
+    def __init__(self, model, encoder, game=None, device=None, temperature=0.0):
         self.engine = TTTGameEngine(model, encoder, device)
-        self.greedy = greedy
+        self.temperature = temperature
 
         if game is None:
             self.game = mechanics.TTTBoard()
@@ -93,7 +97,7 @@ class TTTGamePlayer:
 
         for _ in range(n_plies):
             tokenid, candidate_sequence, _ = self.engine.generate_next_tokenid(
-                self.sequence, greedy=self.greedy, predict_winner=False
+                self.sequence, temperature=self.temperature, predict_winner=False
             )
             self.current_ply += 1
 
@@ -110,7 +114,7 @@ class TTTGamePlayer:
 
     def _predict_end(self):
         tokenid, candidate_sequence, _ = self.engine.generate_next_tokenid(
-            self.sequence, greedy=self.greedy, predict_winner=False
+            self.sequence, temperature=self.temperature, predict_winner=False
         )
         self.current_ply += 1
         if tokenid == self.engine.end_tokenid:
@@ -133,7 +137,7 @@ class TTTGamePlayer:
         self.current_ply += 1
         winner = self.game.winner
         tokenid, _, _ = self.engine.generate_next_tokenid(
-            self.sequence, greedy=self.greedy, predict_winner=True
+            self.sequence, temperature=self.temperature, predict_winner=True
         )
         if winner == self.engine.encoder.inverse_transform([tokenid])[0]:
             self.winner_correctly_predicted = True

rl_train.py
@@ -13,6 +13,7 @@ from chess_seq.tictactoe.evaluation import full_eval
 from torch.utils.tensorboard import SummaryWriter
 
 import chess_seq.utils as utils
+import chess_seq.models as models
 from configs import RLTraining
 
 import shutil
@@ -28,6 +29,8 @@ if __name__ == "__main__":
 
     model, encoder, checkpoint = utils.load_model(session.model_name)
     model_config = checkpoint["model_config"]
+    model = models.ChessNet(config=model_config).to(device)
+
     base_name = f"{model_config.name}_{checkpoint['n_games']}"
 
     logs_path = os.path.join(session.log_dir, session.model_name)
@@ -37,7 +40,7 @@ if __name__ == "__main__":
 
     group_size = 64
     n_groups = 2
-    end_lr_steps = 20000 / (group_size)
+    end_lr_steps = 40000 / (group_size)
 
     eval_frequency = max(1000, group_size * n_groups)
 
@@ -46,15 +49,16 @@ if __name__ == "__main__":
         encoder,
         device=device,
         base_name=base_name,
-        beta=0,
+        beta=0.05,
         epsilon_low=0.1,
-        epsilon_high=0.15,
+        epsilon_high=0.1,
         group_size=group_size,
         n_groups=n_groups,
         learning_rate=5e-5,
         min_lr=1e-5,
         end_lr_steps=end_lr_steps,
         writer=writer,
+        prints=False,
     )
 
     # agent = REINFORCE(
@@ -67,8 +71,8 @@ if __name__ == "__main__":
     # )
 
     # adversary = SimplePlayer()
-    # adversary = RandomPlayer()
-    adversary = ReasonablePlayer()
+    adversary = RandomPlayer()
+    # adversary = ReasonablePlayer()
 
     # adv_model, adv_encoder, _ = utils.load_model(
     #     "ttt_large_573440_GRPO",
@@ -77,10 +81,10 @@ if __name__ == "__main__":
     # )
     # adversary = NNPlayer(adv_model, adv_encoder, mask_illegal=True, device=None)
 
-    env = TTTEnv(adversary, agent_start=True, greedy_adversary=True, illegal_cost=-5)
+    env = TTTEnv(adversary, agent_start=True, adv_temperature=0.5, illegal_cost=-5)
     max_steps = 400_000
 
-    p_start = 1
+    p_start = 1.0
     full_eval(agent, env, writer, N_eval=250, prints=True, p_start=p_start)
 
     start_time = time.time()
@@ -91,7 +95,7 @@ if __name__ == "__main__":
         done = False
         legal_moves = env.game.legal_moves
         while not done:
-            action = agent.get_action(state, greedy=True, legals=legal_moves)
+            action = agent.get_action(state, temperature=0.5, legals=legal_moves)
             next_state, reward, terminated, truncated, info = env.step(action)
             legal_moves = info.get("legal_moves", [])
             done = terminated or truncated