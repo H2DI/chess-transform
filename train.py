60538e1269fa22960b13bc76693bca0000c0eda1
H2DI <hedi.hadiji@gmail.com>
2025-12-03 17:39:30 +0100

    .

--- train.py
+++ train.py
@@ -1,11 +1,18 @@
-from configs import TrainingSession, ModelConfig, TrainingConfig
-from chess_seq import ChessTrainerRunner
 import torch
 
+from configs import TrainingSession, ModelConfig, TrainingConfig
+from chess_seq import ChessTrainerRunner
+
+
+session_config = TrainingSession()
 
 runner = ChessTrainerRunner(
-    session_config=TrainingSession(),
+    session_config=session_config,
     model_config=ModelConfig(),
     training_config=TrainingConfig(),
 )
+
+if session_config.compile:
+    runner.model = torch.compile(runner.model)
+
 runner.train()
